f3881a6fcd0b297d54240af3193aa9a49d325c2d
NamanSaini18 <81188700+NamanSaini18@users.noreply.github.com>
2021-09-19 13:29:22 +0530

    Create First_JAVA_Program.java

---
+++ First_JAVA_Program.java
@@ -0,0 +1,22 @@
+// This is my first Java Program code. This is the most basic program in which we are learning the syntax and other basics of java programming language.
+
+public class Firstclass {                      // Here Firstclass is the name of the class
+    public static void main(String args[]) {           // This is the basic syntax of writing a program in java programming
+        System.out.println("HELLO WORLD");                // This statement is used for output in java programming
+    }                                // In JAVA Programming, We have to end our statement with semicolon(;) unlike python otherwise our program will not execute.
+}
+// Our output will be --> HELLO WORLD
+
+// This is our second basic program of java in which we have tried to print output in different lines. Syntax is same , only we have used three different output statements. We are just trying different things here.
+
+public class Firstclass {
+    public static void main(String args[]) {
+        System.out.println("HELLO WORLD");
+        System.out.println("Hlo guys I am Naman Saini");
+        System.out.println("Welcome to java programming");  // We can also use single quotes inside the parenthesis of output statement but as a convention we will use double quotes everywhere.
+    }
+}
+ // Here O/P will be: 
+//  HELLO WORLD
+//  Hlo guys I am Naman Saini
+//  Welcome to java programming
