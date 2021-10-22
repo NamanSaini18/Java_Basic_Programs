6c23d0b97da8338aa926391bf36e5be4086b79f7
Naman Saini <81188700+NamanSaini18@users.noreply.github.com>
2021-10-23 00:06:15 +0530

    Create Hackerrank_Java Abstarct Class_solution.java

---
+++ Hackerrank_Java Abstarct Class_solution.java
@@ -0,0 +1,8 @@
+// This is the solution of Hackerrank problem named as Java Abstact Class under the topic of Object Oriented Programming
+
+class MyBook extends Book{
+    void setTitle(String s){
+        title = s;
+    }
+    
+}
