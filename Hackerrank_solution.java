837cde24b79c0c5acce358b9ef376e0efea8bf34
Naman Saini <81188700+NamanSaini18@users.noreply.github.com>
2021-10-23 00:13:12 +0530

    Create Hackerrank_solution.java

---
+++ Hackerrank_solution.java
@@ -0,0 +1,30 @@
+// This is the solution of Hackerrank Problem named as Java Method Overriding II 
+import java.util.*;
+import java.io.*;
+
+
+class BiCycle{
+	String define_me(){
+		return "a vehicle with pedals.";
+	}
+}
+
+class MotorCycle extends BiCycle{
+	String define_me(){
+		return "a cycle with an engine.";
+	}
+	
+	MotorCycle(){
+		System.out.println("Hello I am a motorcycle, I am "+ define_me());
+
+		String temp=super.define_me(); // I've only added super keyword here as given in the problem statement, Rest all of the code is the same
+
+		System.out.println("My ancestor is a cycle who is "+ temp );
+	}
+	
+}
+class Solution{
+	public static void main(String []args){
+		MotorCycle M=new MotorCycle();
+	}
+}
