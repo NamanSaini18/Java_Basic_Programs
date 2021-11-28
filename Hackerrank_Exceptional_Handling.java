a47c271d4f5c186bf09a108549f6ba2a19d0fd28
Naman Saini <81188700+NamanSaini18@users.noreply.github.com>
2021-11-28 14:09:18 +0530

    Update Hackerrank_Exceptional_Handling.java

--- Hackerrank_Exceptional_Handling.java
+++ Hackerrank_Exceptional_Handling.java
@@ -6,7 +6,7 @@ class MyCalculator {
         if(n<0 || p<0){
             throw new Exception("n or p should not be negative.");
         }
-        else if(n==0 || p==0){
+        else if(n==0 && p==0){
             throw new Exception("n and p should not be zero.");
             
         }
